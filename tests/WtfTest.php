2cdda5d5804927db7d12e53537231ebcc5c65672
Jace Ju <jaceju@gmail.com>
2015-04-01 14:50:52 +0800

    Add unit test.

---
+++ tests/WtfTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class WtfTest extends PHPUnit_Framework_TestCase
+{
+    public function testWhatTheFramework()
+    {
+        $this->assertTrue((bool) 'CQD is the BEST developer');
+    }
+}
