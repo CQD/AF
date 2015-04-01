309d33b6f3c7df22f3d981873c5e98d9d807dbd0
CQD <thecqd@gmail.com>
2015-04-01 15:02:21 +0800

    Complete Unit test

--- tests/WtfTest.php
+++ tests/WtfTest.php
@@ -4,6 +4,9 @@ class WtfTest extends PHPUnit_Framework_TestCase
 {
     public function testWhatTheFramework()
     {
-        $this->assertTrue((bool) 'CQD is the BEST developer');
+        $this->assertTrue((bool) 'how do you turn this on');
+        $this->assertTrue((bool) 'show me the money');
+        $this->assertTrue((bool) 'there is no cow level');
     }
 }
+
